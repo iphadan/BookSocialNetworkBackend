edfd9aca9718cfbee0da5a28d52fcfa37e209dc9
iphadan <ephadaniel177@gmail.com>
2024-09-26 18:00:29 +0300

    login and registration page built

---
+++ BookNetworkUI/src/app/pages/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component,NgModule } from '@angular/core';
+import {AuthenticationRequest} from '../../services/models/authentication-request';
+import { CommonModule,} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationControllerService } from '../../services/services';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  constructor(private router:Router,private authService:AuthenticationControllerService,){}
+register() {
+this.router.navigate(['register'])
+}
+login() {
+
+}
+authRequest: AuthenticationRequest ={email:'',password:''}
+errorMsg : Array<string> = [];
+
+}
